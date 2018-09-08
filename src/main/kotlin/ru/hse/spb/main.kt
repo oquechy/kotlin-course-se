dfab1100733a591306fb321cd49a5c66ee706213
Elizaveta Vasilenko <vasilliza@gmail.com>
2018-09-08 23:57:04 +0200

    chillin: 51B + unit tests

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,63 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.io.InputStream
 
-    return words.joinToString(separator = " ")
+enum class Token {
+    BEGIN_TABLE,
+    END_TABLE,
+    CELL
+}
+
+fun countCells(tokens: List<Token>, start: Int, counts: MutableList<Int>): Int {
+    var count = 0
+    var i = start
+    while (i < tokens.size) {
+        when (tokens[i]) {
+            Token.CELL -> {
+                count++
+                i++
+            }
+            Token.BEGIN_TABLE -> {
+                i = countCells(tokens, i + 1, counts)
+            }
+            Token.END_TABLE -> {
+                counts.add(count)
+                return i + 1
+            }
+        }
+    }
+    throw IllegalArgumentException("missing </table>")
+}
+
+fun tokenize(source: String): List<Token> {
+    val split = source.split("><", "<", ">")
+    return split.filter { it.isNotBlank() && it != "tr" && it != "/tr" && it != "/td" }.map {
+        when (it) {
+            "table" -> Token.BEGIN_TABLE
+            "/table" -> Token.END_TABLE
+            "td" -> Token.CELL
+            else -> throw IllegalArgumentException("Unrecognized tag: $it")
+        }
+    }
+}
+
+fun readInput(inputStream: InputStream): String {
+    return inputStream.bufferedReader().useLines { lines ->
+        val stringBuilder = StringBuilder()
+        lines.forEach { stringBuilder.append(it) }
+        stringBuilder.toString()
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val source = readInput(System.`in`)
+
+    val tokens = tokenize(source)
+    if (tokens[0] != Token.BEGIN_TABLE) throw IllegalArgumentException("first tag is not <table>: ${tokens[0]}")
+
+    val counts = ArrayList<Int>()
+    val i = countCells(tokens, 1, counts)
+    if (i != tokens.size) throw IllegalArgumentException("tags after end of table")
+
+    counts.sorted().forEach { print("$it ") }
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,73 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
+import com.google.common.truth.Truth.assertThat
+import org.apache.commons.io.IOUtils
 import org.junit.Test
 
 class TestSource {
+
+    companion object {
+        val samples = arrayOf(
+                "<table><tr><td></td></tr></table>\n",
+                "<table>\n" +
+                        "<tr>\n" +
+                        "<td>\n" +
+                        "<table><tr><td></td></tr><tr><td></\n" +
+                        "td\n" +
+                        "></tr><tr\n" +
+                        "><td></td></tr><tr><td></td></tr></table>\n" +
+                        "</td>\n" +
+                        "</tr>\n" +
+                        "</table>\n",
+                "<table><tr><td>\n" +
+                        "<table><tr><td>\n" +
+                        "<table><tr><td>\n" +
+                        "<table><tr><td></td><td></td>\n" +
+                        "</tr><tr><td></td></tr></table>\n" +
+                        "</td></tr></table>\n" +
+                        "</td></tr></table>\n" +
+                        "</td></tr></table>\n")
+
+        val tokens = arrayOf(
+                listOf(Token.BEGIN_TABLE, Token.CELL, Token.END_TABLE),
+                listOf(
+                        Token.BEGIN_TABLE, Token.CELL,
+                        Token.BEGIN_TABLE, Token.CELL, Token.CELL, Token.CELL, Token.CELL,
+                        Token.END_TABLE, Token.END_TABLE),
+                listOf(
+                        Token.BEGIN_TABLE, Token.CELL,
+                        Token.BEGIN_TABLE, Token.CELL,
+                        Token.BEGIN_TABLE, Token.CELL,
+                        Token.BEGIN_TABLE, Token.CELL, Token.CELL, Token.CELL,
+                        Token.END_TABLE, Token.END_TABLE, Token.END_TABLE, Token.END_TABLE))
+        val counts = arrayOf(
+                listOf(1),
+                listOf(1, 4),
+                listOf(1, 1, 1, 3))
+    }
+
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testReadInput() {
+        samples.forEach {
+            val inputStream = IOUtils.toInputStream(it, "UTF-8")
+            assertThat(readInput(inputStream)).isEqualTo(it.replace("\n", ""))
+        }
+    }
+
+    @Test
+    fun testTokenize() {
+        for ((i, it) in samples.withIndex()) {
+            assertThat(tokenize(it.replace("\n", ""))).isEqualTo(tokens[i])
+        }
+    }
+
+    @Test
+    fun testCountCells() {
+        for ((i, it) in tokens.withIndex()) {
+            val result = mutableListOf<Int>()
+            assertThat(countCells(it, 1, result)).isEqualTo(tokens[i].size)
+            assertThat(result).containsExactlyElementsIn(counts[i])
+        }
     }
 }
